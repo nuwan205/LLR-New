7133d8e2208c01f98586c92f1c1fb0ac1856007c
llr-devteam <108239002+llr-devteam@users.noreply.github.com>
2023-03-17 19:35:10 +0530

    Update index.tsx

--- src/pages/register/payment/index.tsx
+++ src/pages/register/payment/index.tsx
@@ -153,7 +153,7 @@ const Payment = () => {
                 >
                   <MenuItem value={process.env.NEXT_PUBLIC_PACKAGE_1 || ''}>Kilimanjaro</MenuItem>
                   <MenuItem value={process.env.NEXT_PUBLIC_PACKAGE_2 || ''}>K2</MenuItem>
-                  {/* <MenuItem value={process.env.NEXT_PUBLIC_PACKAGE_3 || ''}>Everest</MenuItem> */}
+                  <MenuItem value={process.env.NEXT_PUBLIC_PACKAGE_3 || ''}>Everest</MenuItem>
                 </Select>
                 <Grid sx={{ my: 6.5 }} container justifyContent='end' alignItems='center'>
                   <Typography variant='subtitle1' component='span' sx={{ pr: 10 }}>
